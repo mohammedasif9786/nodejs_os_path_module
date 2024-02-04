d0e9bebbfee45a7b8fc3c8a5cfa9407bf98de4e1
mohammedasif9786 <asif.jdp@gmail.com>
2024-02-04 22:23:09 +0530

    nodejs OS & path module

---
+++ osModule.js
@@ -0,0 +1,4 @@
+let operatSys = require('os')
+console.log(operatSys.type())
+console.log(operatSys.version())
+
---
+++ pathModule.js
@@ -0,0 +1,5 @@
+let pathModule = require('path')
+console.log(pathModule.dirname(__filename))
+console.log(pathModule.basename(__filename))
+console.log(pathModule.extname(__filename))
+
